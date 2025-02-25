eacb93990e03dc37631272af9602454b37db33db
Whit Huntley <whuntle5@charlotte.edu>
2025-02-24 19:59:13 -0500

    test(backend): Add notes for book unmarshaler

--- pkg/models/book_whitebox_test.go
+++ pkg/models/book_whitebox_test.go
@@ -57,7 +57,6 @@ func TestJSONMarshal(t *testing.T) {
 		}
 	}
 
-	// For reasons beyond our understanding this puts each object as... base64???
 	_, err := json.Marshal(booksExample)
 	if err != nil {
 		t.Errorf("unable to marshal into slice: %s", err)
@@ -65,8 +64,9 @@ func TestJSONMarshal(t *testing.T) {
 }
 
 /*func TestJSONUnmarshal(t *testing.T) {
-	singleJsonBooks := [][]byte{[]byte(`{"id":"01953a93-21e7-73da-8a27-fc22aa66a95e","isbns":[{"type":"isbn10","value":"0141439602"},{"type":"isbn13","value":"9780141439600"}],"title":"A Tale of Two Cities","author":"Charles Dickens","published":"1859-11-26"}`),
+	individualBooksJson := [][]byte{[]byte(`{"id":"01953a93-21e7-73da-8a27-fc22aa66a95e","isbns":[{"type":"isbn10","value":"0141439602"},{"type":"isbn13","value":"9780141439600"}],"title":"A Tale of Two Cities","author":"Charles Dickens","published":"1859-11-26"}`),
 		[]byte(`{"id":"01953a93-21e7-73dd-84c7-a4df7fadac5a","isbns":[{"type":"isbn10","value":"0156012197"},{"type":"isbn13","value":"9780156012195"}],"title":"The Little Prince","author":"Antoine de Saint-Exupéry","published":"1943-04-00"}`),
 		[]byte(`{"id":"01953a93-21e7-73de-84dd-33f54daba1ec","isbns":[{"type":"isbn10","value":"0062315005"},{"type":"isbn13","value":"9780061122415"}],"title":"The Alchemist","author":"Paulo Coelho","published":"1988-00-00"}`)}
 
+	listBooksJson := []byte([{"id":"01953a9c-55a1-7c04-b0f1-71c51c1cc8c5","isbns":[{"type":"isbn10","value":"0141439602"},{"type":"isbn13","value":"9780141439600"}],"title":"A Tale of Two Cities","author":"Charles Dickens","published":"1859-11-26"},{"id":"01953a9c-55a1-7c07-9576-05c0deb47fe5","isbns":[{"type":"isbn10","value":"0156012197"},{"type":"isbn13","value":"9780156012195"}],"title":"The Little Prince","author":"Antoine de Saint-Exupéry","published":"1943-04-00"},{"id":"01953a9c-55a1-7c08-be47-6530a96888ae","isbns":[{"type":"isbn10","value":"0062315005"},{"type":"isbn13","value":"9780061122415"}],"title":"The Alchemist","author":"Paulo Coelho","published":"1988-00-00"}])
 }*/
