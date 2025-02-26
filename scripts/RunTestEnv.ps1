690b7035dfdfc43b24210c062d923f8ecd61dd8a
Omoikhoa <alexilevbare.com>
2025-02-26 08:01:36 -0500

    fix: updated PowerShell script

--- scripts/RunTestEnv.ps1
+++ scripts/RunTestEnv.ps1
@@ -17,23 +17,29 @@ Set-Location $repoRoot
 
 # Build image with timestamp tag
 $jawsTag = Get-Date -Format "yyyyMMddHHmmss"
-docker build --target webnative -t "jaws:$jawsTag" .
+docker build --target app -t "jaws-app:$jawsTag" .
+docker build --target psql -t "jaws-psql:$jawsTag" .
+
+# Create Docker network
+docker network create --driver bridge jaws-net -ErrorAction SilentlyContinue
 
 # Run containers with automatic cleanup (--rm) and default ports
 docker run --rm --name jaws-psql `
+    --network jaws-net `
     -p 54321:5432 `
     -e POSTGRES_USER="jaws" `
     -e POSTGRES_DB="jaws" `
     -e POSTGRES_PASSWORD=$psql_passwd `
-    -d postgres:17-alpine
+    -d jaws-psql:$jawsTag
 
 docker run --rm --name jaws-app `
+    --network jaws-net `
     -p 8080:80 `
     -e PG_PASSWORD=$psql_passwd `
     -e PG_USER="jaws" `
-    -e PG_DATABASE="jaws" `
+    -e PG_DB="jaws" `
     -e PG_HOST="jaws-psql" `
-    -e PG_PORT=54321 `
-    -d "jaws:$jawsTag"
+    -e PG_PORT=5432 `
+    -d jaws-app:$jawsTag
 
 Write-Host "`nContainers running!`nApp: http://localhost:8080`nPostgres: localhost:54321`n"
