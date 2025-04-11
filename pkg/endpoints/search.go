bcb0fa13333041998fc077a629ab627b902ec0cb
Whit Huntley <whuntle5@charlotte.edu>
2025-04-11 02:26:09 -0400

    feat(backend): search params set?

--- pkg/endpoints/search.go
+++ pkg/endpoints/search.go
@@ -1,6 +1,9 @@
 package endpoints
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/whit-colm/itsc-4155-project/pkg/repository"
@@ -13,5 +16,27 @@ type searchHandle[S comparable] struct {
 }
 
 func (h searchHandle[S]) Search(c *gin.Context) {
+	const errorCaller string = "search"
+	// Do search query stuff
+
+	var (
+		idx    []string
+		query  string // TODO: "[]S"?
+		limit  int
+		offset int
+	)
+	idx = strings.Split(c.Param("idx"), ",")
+	query = c.Param("q")
+	if r, err := strconv.Atoi(c.Param("r")); err != nil {
+		limit = 25
+	} else {
+		limit = r
+	}
+	if o, err := strconv.Atoi(c.Param("o")); err != nil {
+		offset = 0
+	} else {
+		offset = o
+	}
+
 	panic("unimplemented")
 }
