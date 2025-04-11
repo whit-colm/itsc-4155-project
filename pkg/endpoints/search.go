8e6d0e6cfa8cfe788691124d7ffebaec99ff6782
Whit Huntley <whuntle5@charlotte.edu>
2025-04-11 02:41:21 -0400

    feat(backend): search boilerplate (2)

--- pkg/endpoints/search.go
+++ pkg/endpoints/search.go
@@ -1,6 +1,9 @@
 package endpoints
 
 import (
+	"encoding/json"
+	"net/http"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -20,12 +23,12 @@ func (h searchHandle[S]) Search(c *gin.Context) {
 	// Do search query stuff
 
 	var (
-		idx    []string
-		query  string // TODO: "[]S"?
-		limit  int
-		offset int
+		domains []string
+		query   string // TODO: "[]S"?
+		limit   int
+		offset  int
 	)
-	idx = strings.Split(c.Param("idx"), ",")
+	domains = strings.Split(c.Param("idx"), ",")
 	query = c.Param("q")
 	if r, err := strconv.Atoi(c.Param("r")); err != nil {
 		limit = 25
@@ -38,5 +41,28 @@ func (h searchHandle[S]) Search(c *gin.Context) {
 		offset = o
 	}
 
-	panic("unimplemented")
+	// Make slices for each search domain
+	if slices.Contains(domains, "comments") {
+		// Search comments
+	}
+	if slices.Contains(domains, "booktitle") {
+		// Search book titles
+	}
+	if slices.Contains(domains, "authorname") {
+		// Search author names
+	}
+
+	ret := make([]*any, 25)
+	for range limit {
+		// Effectively do the merge part of merge sort
+		// Because each slice will already be sorted by best scoring
+		// first, we simply pick which top value of the search domains
+		// has the highest out of all of them, add it to the `ret`
+		// slice, then pop said slice.
+	}
+	retJson, err := json.Marshal(ret)
+	if err != nil {
+		panic(err) // TODO: Not this
+	}
+	c.JSON(http.StatusOK, retJson)
 }
